08c070811b64b27ffb389f1f607e11a8fcfc1859
#TECH EMPRESS$ <marynjoki738@gmail.com>
2022-09-08 13:09:19 +0300

    made changes to index.html

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import Header from './LandingPage/Header';
+import Reviews from './LandingPage/Reviews';
+import About from './LandingPage/About'
+import Footer from './LandingPage/Footer';
+import Dashboard from './Admin/Dashboard';
+import Chart from './Admin/DashboardChart'
+import Login from './Login/Login'
+import Profile from './Profile/Profile'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+      {/* <Navbar/> */}
+      {/* <Header/> */}
+      {/* <Reviews/> */}
+      {/* <About/> */}
+      {/* <Footer/> */}
+      {/* <Dashboard/> */}
+      {/* <Login/> */}
+      <Profile/>
+
+
     </div>
   );
 }
