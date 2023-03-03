6bf2ea8af6d617b279441ddba8282c06a498b3a7
ELIJAH KIRAGU <eliki13720@gmail.com>
2023-03-03 11:56:40 +0300

    connected all backend endpoints

---
+++ src/API/api.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+
+const mainUrl = 'http://127.0.0.1:8000/'
+
+const registrationUrl =`${mainUrl}/registration`
+const loginUrl =`${mainUrl}/login`
+const userUpdateUrl =`${mainUrl}//update_user/<pk>/`
+const getAllUsersUrl =`${mainUrl}/all_users/`
+const viewPostUrl = `${mainUrl}/api/post/`
+const postLikeUrl = `${mainUrl}/posts/<int:pk>/like/`
+const postUnlikeUrl =`${mainUrl}/posts/<int:pk>/unlike/`
+const commentUrl =`${mainUrl}/comments/`
